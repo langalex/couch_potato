5199036e4f5efecbcbb02764528369207ad2bbb8
priit.mx.ee <priit@mx.ee>
2008-11-15 18:58:37 +0200

    added helpful line to log when potato is initialized

--- rails/init.rb
+++ rails/init.rb
@@ -3,3 +3,5 @@
 require File.dirname(__FILE__) + '/../lib/couch_potato'
 
 CouchPotato::Config.database_name = YAML::load(File.read(RAILS_ROOT + '/config/couchdb.yml'))[RAILS_ENV]
+
+RAILS_DEFAULT_LOGGER.info "** couch_potato: initialized from #{__FILE__}"
