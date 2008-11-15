1495298e5fb51ed80d5aa94ce2ae2311e08c5ec5
priit.mx.ee <priit@mx.ee>
2008-11-15 18:55:50 +0200

    added gem initialization for rails + readme update

---
+++ rails/init.rb
@@ -0,0 +1,5 @@
+# this is for rails only
+
+require File.dirname(__FILE__) + '/../lib/couch_potato'
+
+CouchPotato::Config.database_name = YAML::load(File.read(RAILS_ROOT + '/config/couchdb.yml'))[RAILS_ENV]
