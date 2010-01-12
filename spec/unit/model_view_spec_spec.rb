6991a3f6a44ca0aa556040f421aed335dfc2b95d
Alexander Lang <alex@upstream-berlin.com>
2010-01-12 20:43:44 +0100

    moved specs to correct location

--- spec/unit/model_view_spec_spec.rb
+++ spec/unit/model_view_spec_spec.rb
@@ -10,31 +10,4 @@ describe CouchPotato::View::ModelViewSpec, 'map_function' do
     spec = CouchPotato::View::ModelViewSpec.new Object, 'all', {}, {}
     spec.map_function.should include('if(doc.ruby_class && doc.ruby_class == \'Object\')')
   end
-  
-  it "should raise an error when passing invalid view parameters" do
-    lambda {
-      CouchPotato::View::ModelViewSpec.new Object, 'all', {}, {:start_key => '1'}
-    }.should raise_error(ArgumentError, "invalid view parameter: start_key")
-  end
-  
-  it "should not raise an error when passing valid view parameters" do
-    lambda {
-      CouchPotato::View::ModelViewSpec.new Object, 'all', {}, {
-        :key => 'keyvalue',
-        :startkey => 'keyvalue',
-        :startkey_docid => 'docid',
-        :endkey => 'keyvalue',
-        :endkey_docid => 'docid',
-        :limit => 3,
-        :stale => 'ok',
-        :descending => true,
-        :skip => 1,
-        :group => true,
-        :group_level => 1,
-        :reduce => false,
-        :include_docs => true,
-        :inclusive_end => true
-      }
-    }.should_not raise_error
-  end
 end
