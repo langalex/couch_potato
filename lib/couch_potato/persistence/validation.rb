d8b032d5420c5a27598254a424f5f2ea37c61c64
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-01-28 00:46:02 +0100

    raise an error when an unsupported validation_framework is configured

--- lib/couch_potato/persistence/validation.rb
+++ lib/couch_potato/persistence/validation.rb
@@ -22,6 +22,8 @@ module CouchPotato
           end
         when :active_model
           base.send :include, ::ActiveModel::Validations
+        else
+          raise "Unknown CouchPotato::Config.validation_framework #{CouchPotato::Config.validation_framework.inspect}, options are :validatable or :active_model"
         end
 
       end
--- spec/unit/validation_spec.rb
+++ spec/unit/validation_spec.rb
@@ -1,44 +1,59 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-begin
-  require 'active_model'
-  
-  describe 'With CouchPotato::Config.validation_framework = :active_model' do
-
-    before(:all) do
-      @original_validation_framework = CouchPotato::Config.validation_framework
-      CouchPotato::Config.validation_framework = :active_model
-    end
-    after(:all) do
-      CouchPotato::Config.validation_framework = @original_validation_framework
-    end
-
-    it "should include ActiveModel::Validations upon inclusion of CouchPotato::Persistence" do
-      model_class = Class.new
-      ActiveModel::Validations.should_receive(:included).with(model_class)
-      model_class.send(:include, CouchPotato::Persistence)
-    end
-
-  end
-  
-rescue LoadError
-  STDERR.puts "WARNING: active_model gem not installed. Not running ActiveModel validation specs."
-end
-
-describe 'With CouchPotato::Config.validation_framework = :validatable' do
-
+describe 'CouchPotato::Config.validation_framework' do
   before(:all) do
     @original_validation_framework = CouchPotato::Config.validation_framework
-    CouchPotato::Config.validation_framework = :validatable
   end
   after(:all) do
     CouchPotato::Config.validation_framework = @original_validation_framework
   end
 
-  it "should include ActiveModel::Validations upon inclusion of CouchPotato::Persistence" do
-    model_class = Class.new
-    Validatable.should_receive(:included).with(model_class)
-    model_class.send(:include, CouchPotato::Persistence)
+  begin
+    require 'active_model'
+
+    describe 'with :active_model' do
+      before(:each) do
+        CouchPotato::Config.validation_framework = :active_model
+      end
+
+      it "should include ActiveModel::Validations upon inclusion of CouchPotato::Persistence" do
+        model_class = Class.new
+        ActiveModel::Validations.should_receive(:included).with(model_class)
+        model_class.send(:include, CouchPotato::Persistence)
+      end
+    end
+
+  rescue LoadError
+    STDERR.puts "WARNING: active_model gem not installed. Not running ActiveModel validation specs."
   end
 
+  begin
+    require 'validatable'
+
+    describe 'with :validatable' do
+      before(:each) do
+        CouchPotato::Config.validation_framework = :validatable
+      end
+
+      it "should include ActiveModel::Validations upon inclusion of CouchPotato::Persistence" do
+        model_class = Class.new
+        Validatable.should_receive(:included).with(model_class)
+        model_class.send(:include, CouchPotato::Persistence)
+      end
+    end
+
+  rescue LoadError
+    STDERR.puts "WARNING: validatable gem not installed. Not running Validatable validation specs."
+  end
+
+  describe 'with an unknown framework' do
+    before(:each) do
+      CouchPotato::Config.validation_framework = :unknown_validation_framework
+    end
+
+    it "should raise an error" do
+      model_class = Class.new
+      lambda { model_class.send(:include, CouchPotato::Persistence) }.should raise_error
+    end
+  end
 end
