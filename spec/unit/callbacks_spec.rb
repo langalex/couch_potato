c4f81e6c955eaae50f3779132f95d1be39e443b4
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-01-28 22:03:18 +0100

    run Callbacks unit spec for both supported validation frameworks (if installed)

--- spec/unit/callbacks_spec.rb
+++ spec/unit/callbacks_spec.rb
@@ -1,32 +1,48 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-class Tree
-  include CouchPotato::Persistence
-
-  before_validation :water!, lambda {|tree| tree.root_count += 1 }
-  
-  property :leaf_count
-  property :root_count
-  
-  def water!
-    self.leaf_count += 1
-  end
-end
-
 describe 'before_validation callback' do
-  before :each do
-    @tree = Tree.new(:leaf_count => 1, :root_count => 1)
+  before(:each) do
+    @original_validation_framework = CouchPotato::Config.validation_framework
   end
-  
-  it "should call water! when validated" do
-    @tree.leaf_count.should == 1
-    @tree.should be_valid
-    @tree.leaf_count.should == 2
+  after(:each) do
+    CouchPotato::Config.validation_framework = @original_validation_framework
   end
+  [:validatable, :active_model].each do |validation_framework|
+    describe "with #{validation_framework}" do
+      before(:each) do
+        CouchPotato::Config.validation_framework = validation_framework
+        @tree = Tree.new(:leaf_count => 1, :root_count => 1)
+      end
+
+      begin
+        Object.send(:remove_const, :Tree) if Object.const_defined?(:Tree)
+        class Tree
+          include CouchPotato::Persistence
+
+          before_validation :water!, lambda {|tree| tree.root_count += 1 }
   
-  it "should call lambda when validated" do
-    @tree.root_count.should == 1
-    @tree.should be_valid
-    @tree.root_count.should == 2
+          property :leaf_count
+          property :root_count
+  
+          def water!
+            self.leaf_count += 1
+          end
+        end
+
+        it "should call water! when validated" do
+          @tree.leaf_count.should == 1
+          @tree.should be_valid
+          @tree.leaf_count.should == 2
+        end
+
+        it "should call lambda when validated" do
+          @tree.root_count.should == 1
+          @tree.should be_valid
+          @tree.root_count.should == 2
+        end
+      rescue LoadError
+        STDERR.puts "WARNING: Skipping Callback unit tests with #{validation_framework} as it is not installed."
+      end
+    end
   end
 end
