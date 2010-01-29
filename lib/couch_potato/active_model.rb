13643bd3db83b13ca956691733adb60ca2148590
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-01-29 10:56:08 +0100

    add ActiveModel compatibility to models only when ActiveModel is installed (otherwise causes problems when a Rails 2 app would try to resolve ActiveModel::Name)

--- lib/couch_potato/active_model.rb
+++ lib/couch_potato/active_model.rb
@@ -1,54 +1,57 @@
-begin
-  require 'active_model'
-rescue LoadError
-  # if it's not installed you probably don't want to use it anyway
-end
+module CouchPotato::ActiveModel
+  begin
+    require 'active_model'
 
-if CouchPotato::Config.validation_framework == :validatable
-  # add [] method to Validatable's implementation of the Errors class
-  module Validatable
-    class Errors
-      def [](attribute)
-        [on(attribute)].flatten.compact
+    if CouchPotato::Config.validation_framework == :validatable
+      # add [] method to Validatable's implementation of the Errors class
+      module Validatable
+        class Errors
+          def [](attribute)
+            [on(attribute)].flatten.compact
+          end
+        end
       end
     end
+
+    class ModelName < String
+      attr_reader :name, :human, :partial_path, :singular, :plural
+      def initialize(class_name)
+        @name = class_name
+        @human = class_name.gsub(/[A-Z]/) {|match| " #{match}"}.strip
+        @partial_path
+        @singular
+        @plural
+      end
+    end
+
+    def self.included(base)
+      base.extend ClassMethods
+      base.class_eval do
+        alias_method :new_record?, :new?
+      end
+    end
+
+    def to_model
+      self
+    end
+
+    def errors
+      super || []
+    end
+
+    def destroyed?
+      !!_deleted
+    end
+
+    module ClassMethods
+      def model_name
+        @model_name ||= ActiveModel::Name.new(self)
+      end
+    end
+
+  rescue LoadError
+    # if it's not installed you probably don't want to use it anyway
   end
 end
 
-module CouchPotato::ActiveModel
-  class ModelName < String
-    attr_reader :name, :human, :partial_path, :singular, :plural
-    def initialize(class_name)
-      @name = class_name
-      @human = class_name.gsub(/[A-Z]/) {|match| " #{match}"}.strip
-      @partial_path
-      @singular
-      @plural
-    end
-  end
-  
-  def self.included(base)
-    base.extend ClassMethods
-    base.class_eval do
-      alias_method :new_record?, :new?
-    end
-  end
-  
-  def to_model
-    self
-  end
-  
-  def errors
-    super || []
-  end
 
-  def destroyed?
-    !!_deleted
-  end
-
-  module ClassMethods
-    def model_name
-      @model_name ||= ActiveModel::Name.new(self)
-    end
-  end
-end
