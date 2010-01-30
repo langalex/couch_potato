38226a1c9529c22653143001bd58484a95035e3e
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-01-31 00:12:44 +0100

    silence deprecation warnings from ActiveModel

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,13 @@ require 'couch_potato'
 CouchPotato::Config.database_name = 'couch_potato_test'
 CouchPotato::Config.validation_framework = ENV['VALIDATION_FRAMEWORK'].to_sym unless ENV['VALIDATION_FRAMEWORK'].blank?
 
+# silence deprecation warnings from ActiveModel as the Spec uses Errors#on
+begin
+  ActiveSupport::Deprecation.silenced = true
+rescue
+  # ignore errors, ActiveSupport is probably not installed
+end
+
 class Child
   include CouchPotato::Persistence
 
