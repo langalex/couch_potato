888a72cdcfe0470574956b6d9c28acfb4688f572
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-02-09 13:17:05 +0100

    refactor Rails init code

--- lib/couch_potato/railtie.rb
+++ lib/couch_potato/railtie.rb
@@ -1,14 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../rails/reload_classes')
 
 module CouchPotato
+
+  def self.rails_init
+    CouchPotato::Config.database_name = YAML::load(File.read(Rails.root.join('config/couchdb.yml')))[Rails.env]
+  end
+
   if Rails.version >= '3'
     class Railtie < Rails::Railtie
       railtie_name :couch_potato
 
       config.after_initialize do |app|
-        CouchPotato::Config.database_name = YAML::load(File.read(Rails.root.join('config/couchdb.yml')))[Rails.env]
-        Rails.logger.info "** couch_potato: initialized from #{__FILE__}"
+        CouchPotato.rails_init
       end
     end
+  else
+    rails_init
   end
+
 end
--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,4 @@
 # this is for rails only
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/couch_potato')
-
-CouchPotato::Config.database_name = YAML::load(File.read(Rails.root.to_s + '/config/couchdb.yml'))[Rails.env]
-
 Rails.logger.info "** couch_potato: initialized from #{__FILE__}"
