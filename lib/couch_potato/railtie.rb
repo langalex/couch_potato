99d68f11697e90c00a7d7c875283b6110bdeadde
Jonathan Weiss <jw@innerewut.de>
2010-09-16 20:46:49 +0800

    do not use Rails.env in initializer as it will free Rails.env for all times and in Rails 2.3.x apps it will be called too early thus always beeing development

--- lib/couch_potato/railtie.rb
+++ lib/couch_potato/railtie.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../rails/reload_classes')
 module CouchPotato
 
   def self.rails_init
-    CouchPotato::Config.database_name = YAML::load(File.read(Rails.root.join('config/couchdb.yml')))[Rails.env]
+    CouchPotato::Config.database_name = YAML::load(File.read(Rails.root.join('config/couchdb.yml')))[RAILS_ENV]
   end
 
   if defined?(::Rails::Railtie)
