9bc80d1193cf32e68530f81f28138b5864573158
Benjamin ter Kuile <bterkuile@gmail.com>
2011-05-22 21:37:04 +0200

    move validation test to unit

--- spec/unit/validation_spec.rb
+++ spec/unit/validation_spec.rb
@@ -65,3 +65,16 @@ describe 'CouchPotato::Config.validation_framework' do
     end
   end
 end
+
+describe Comment do
+  it "Should not be valid when newly created" do
+    comment = Comment.new
+    comment.should_not be_valid
+  end
+
+  it "should be valid with a title" do
+    comment = Comment.new :title => "I am valid"
+    comment.should be_valid
+  end
+
+end
