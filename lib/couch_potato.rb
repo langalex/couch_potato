1aa1b5f4c7ac1b0913fe8e0ee830bc2d0188b4ab
Simone Carletti <weppos@weppos.net>
2011-09-16 14:21:36 +0200

    Fix error no such file to load — json/add/rails
    See http://stackoverflow.com/questions/7442788/couch-potato-no-such-file-to-load-json-add-rails/7444162#7444162

--- lib/couch_potato.rb
+++ lib/couch_potato.rb
@@ -1,7 +1,7 @@
 require 'couchrest'
 require 'json'
 require 'json/add/core'
-require 'json/add/rails'
+require 'json/add/rails' rescue nil
 
 require 'ostruct'
 
@@ -11,9 +11,9 @@ module CouchPotato
   Config = Struct.new(:database_name, :validation_framework, :split_design_documents_per_view).new
   Config.validation_framework = :active_model
   Config.split_design_documents_per_view = false
-  
+
   class NotFound < StandardError; end
-  
+
   # returns all the classes that implement the CouchPotato::Persistence module
   def self.models
     @models ||= []
