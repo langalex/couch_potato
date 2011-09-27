502b748b719971be16b6e138b1b99bb55ce13fb3
Alexander Lang <alex@upstre.am>
2011-09-27 21:35:17 +0200

    fix specs hang when all are run together

--- spec/railtie_spec.rb
+++ spec/railtie_spec.rb
@@ -10,7 +10,7 @@ module Rails
     def self.initializer(*args)
     end
   end
-  
+
   def self.root
     RSpec::Mocks::Mock.new :join => ''
   end
@@ -21,45 +21,47 @@ require 'couch_potato/railtie'
 describe "railtie" do
   before(:all) do
     @validation_framework = CouchPotato::Config.validation_framework
+    @database_name = CouchPotato::Config.database_name
   end
-  
+
   after(:all) do
     CouchPotato::Config.validation_framework = @validation_framework
+    CouchPotato::Config.database_name = @database_name
   end
-  
+
   context 'yaml file contains only database names' do
     it "should set the database name from the yaml file" do
       File.stub(:read => "test: test_db")
-      
+
       CouchPotato::Config.should_receive(:database_name=).with('test_db')
-      
+
       CouchPotato.rails_init
     end
   end
-  
+
   context 'yaml file contains more configuration' do
     before(:each) do
       File.stub(:read => "test: \n  database: test_db\n  validation_framework: :active_model")
     end
-    
+
     it "should set the database name from the yaml file" do
       CouchPotato::Config.should_receive(:database_name=).with('test_db')
-      
+
       CouchPotato.rails_init
     end
-    
+
     it "should set the validation framework from the yaml file" do
       CouchPotato::Config.should_receive(:validation_framework=).with(:active_model)
-      
+
       CouchPotato.rails_init
     end
   end
-  
+
   it "should process the yml file with erb" do
     File.stub(:read => "test: \n  database: <%= 'db' %>")
 
     CouchPotato::Config.should_receive(:database_name=).with('db')
-    
+
     CouchPotato.rails_init
   end
 end
