ef5932353848a3965853532394f4b0b4aa1ad6fa
Alexander Lang <alex@upstre.am>
2019-05-30 15:21:50 +0200

    add support for default validation context validations that use e.g.  or  were not run before. now they are. fix #132

--- spec/unit/validation_spec.rb
+++ spec/unit/validation_spec.rb
@@ -1,13 +1,53 @@
 require 'spec_helper'
 
 describe 'CouchPotato Validation' do
-  
+
   describe "access to errors object" do
-    it "should description" do
+    it "adds an errors method the the errors object to be compatible with Validatable" do
       model_class = Class.new
       model_class.send(:include, CouchPotato::Persistence)
       expect(model_class.new.errors).to respond_to(:errors)
     end
   end
 
+  class Monkey
+    include CouchPotato::Persistence
+    property :create_property
+    property :update_property
+
+    validates :create_property, presence: true, on: :create
+    validates :update_property, presence: true, on: :update
+  end
+
+  context 'create' do
+    let(:monkey) { Monkey.new }
+
+    before do
+      monkey.valid?
+    end
+
+    it 'has an error on the create property' do
+      expect(monkey.errors[:create_property]).to be_present
+    end
+
+    it 'has no error on the update property' do
+      expect(monkey.errors[:update_property]).not_to be_present
+    end
+  end
+
+  context 'update' do
+    let(:monkey) { Monkey.new _rev: '1' }
+
+    before do
+      monkey.valid?
+    end
+
+    it 'has no error on the create property' do
+      expect(monkey.errors[:create_property]).to_not be_present
+    end
+
+    it 'has an error on the update property' do
+      expect(monkey.errors[:update_property]).to be_present
+    end
+  end
 end
