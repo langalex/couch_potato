a8c922f354fd96daec2ad70326765192f5c1057d
Alexander Lang <2173+langalex@users.noreply.github.com>
2022-10-31 13:49:51 +0100

    Ruby 3.1 (#157)
    Add Ruby 3.1 support

--- lib/couch_potato/railtie.rb
+++ lib/couch_potato/railtie.rb
@@ -8,7 +8,10 @@ module CouchPotato
     path = Rails.root.join('config/couchdb.yml')
     if File.exist?(path)
       require 'yaml'
-      config = YAML.safe_load(ERB.new(File.read(path)).result, [Symbol], [], ['default'])[Rails.env]
+      config = YAML.safe_load(
+        ERB.new(File.read(path)).result, 
+        permitted_classes: [Symbol], 
+      )[Rails.env]
       CouchPotato.configure(config)
     else
       Rails.logger.warn 'Rails.root/config/couchdb.yml does not exist. Not configuring a database.'
