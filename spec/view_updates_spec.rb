a1596fabb89e77525eae5424d9aa8bcde4b84a46
Alexander Lang <alex@cobot.me>
2025-06-03 14:36:57 +0200

    fix specs

--- spec/view_updates_spec.rb
+++ spec/view_updates_spec.rb
@@ -7,8 +7,27 @@ describe "automatic view updates" do
   end
   
   it "should update a view that doesn't match the given functions" do
-    CouchPotato::View::ViewQuery.new(@db, 'test_design1', {'test_view' => {:map => 'function(doc) {}', :reduce => 'function() {}'}}, nil).query_view! # create view
-    CouchPotato::View::ViewQuery.new(@db, 'test_design1', {'test_view' => {:map => 'function(doc) {emit(doc.id, null)}', :reduce => 'function(key, values) {return sum(values)}'}}, nil).query_view!
+    CouchPotato::View::ViewQuery.new(
+      @db, 
+      'test_design1', {
+        'test_view' => {
+          :map => 'function(doc) { }',
+          :reduce => 'function(key, values) { return []; }'
+        }
+      }
+    ).query_view! # create view
+    CouchPotato::View::ViewQuery.clear_cache
+    CouchPotato::View::ViewQuery.new(
+      @db,
+      'test_design1', 
+      {
+        'test_view' => {
+          :map => 'function(doc) {emit(doc.id, null)}',
+          :reduce => 'function(key, values) {return sum(values)}'
+        }
+      }
+    ).query_view!
+
     expect(CouchPotato.database.load('_design/test_design1')['views']['test_view']).to eq({
       'map' => 'function(doc) {emit(doc.id, null)}',
       'reduce' => 'function(key, values) {return sum(values)}'
@@ -16,9 +35,38 @@ describe "automatic view updates" do
   end
   
   it "should only update a view once to avoid writing the view for every request" do
-    CouchPotato::View::ViewQuery.new(@db, 'test_design2', {'test_view' => {:map => 'function(doc) {}', :reduce => 'function() {}'}}, nil).query_view! # create view
-    CouchPotato::View::ViewQuery.new(@db, 'test_design2', {'test_view' => {:map => 'function(doc) {emit(doc.id, null)}', :reduce => 'function(key, values) {return sum(values)}'}}, nil).query_view!
-    CouchPotato::View::ViewQuery.new(@db, 'test_design2', {'test_view' => {:map => 'function(doc) {}', :reduce => 'function() {}'}}, nil).query_view!
+    CouchPotato::View::ViewQuery.new(
+      @db,
+      'test_design2',
+      {
+        'test_view' => {
+          :map => 'function(doc) {}',
+          :reduce => 'function() {}'
+        }
+      }
+    ).query_view! # create view
+    CouchPotato::View::ViewQuery.clear_cache
+    CouchPotato::View::ViewQuery.new(
+      @db,
+      'test_design2',
+      {
+        'test_view' => {
+          :map => 'function(doc) {emit(doc.id, null)}',
+          :reduce => 'function(key, values) {return sum(values)}'
+        }
+      }
+    ).query_view!
+    CouchPotato::View::ViewQuery.new(
+      @db,
+      'test_design2',
+      {
+        'test_view' => {
+          :map => 'function(doc) {}',
+          :reduce => 'function() {}'
+        }
+      }
+    ).query_view!
+
     expect(CouchPotato.database.load('_design/test_design2')['views']['test_view']).to eq({
       'map' => 'function(doc) {emit(doc.id, null)}',
       'reduce' => 'function(key, values) {return sum(values)}'
