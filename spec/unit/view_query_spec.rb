c0882707b039396c7ca8e99680de97e37c258cec
Alexander Lang <alex@cobot.me>
2025-06-04 18:11:52 +0200

    add digest to single design doc name
    this way, instead of updating the existing one (and blocking all view queries during that time) during a deployment, a new design doc is created when any views are changed.

--- spec/unit/view_query_spec.rb
+++ spec/unit/view_query_spec.rb
@@ -8,6 +8,11 @@ describe CouchPotato::View::ViewQuery, 'query_view!' do
     CouchPotato::View::ViewQuery.clear_cache
   end
 
+  after(:each) do
+    CouchPotato::Config.single_design_document = false
+    CouchPotato::Config.digest_view_names = false
+  end
+
   it 'does not pass a key if conditions are empty' do
     expect(db).to receive(:view).with(anything, {})
     CouchPotato::View::ViewQuery.new(db, '', {:view0 => {}}).query_view!
@@ -80,5 +85,24 @@ describe CouchPotato::View::ViewQuery, 'query_view!' do
     allow(db).to receive(:get).and_return({'views' => {'view8' => {'map' => '<map_code>', 'reduce' => '<reduce_code>'}}})
     expect(db).to receive(:save_doc)
     CouchPotato::View::ViewQuery.new(db, 'design', :view8 => {:map => '<map_code>', :reduce => '<new reduce_code>'}).query_view!
+  end
+
+  it 'adds a digest of all views to the design document if single_design_doc is true' do
+    CouchPotato::Config.single_design_document = true
+    CouchPotato::Config.digest_view_names = true
+
+    allow(db).to receive(:get).and_return(nil)
+    allow(db).to receive(:save_doc).and_return(true)
+    view_class = double('view_class',
+      views: {view: {map: '<map_code>'}},
+      execute_view: double('view_spec', view_name: 'view', map_function: '<map_code>', reduce_function: nil))
+    allow(CouchPotato).to receive(:views).and_return([view_class])
+
+    CouchPotato::View::ViewQuery.new(
+      db, 
+      'couch_potato',
+      {:view => {:map => '<map_code>'}}).query_view!
+
+    expect(db).to have_received(:save_doc).with(hash_including({"_id" => "_design/couch_potato-56d286b4f0cd3a50fdd2ad428034d08a6483311539f7e138c45e781611b9dbbc"}))
   end
 end
